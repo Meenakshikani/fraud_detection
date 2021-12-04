181770e7bd1440a2c8dd575e1b6e1ff791e64716
Meenakshikani <meenakshi02232020@gmail.com>
2021-12-04 00:18:18 -0600

    sum function added

--- Data_Processing.py
+++ Data_Processing.py
@@ -1,1 +1,10 @@
 print('adding all the data processing related to fraud detection')
+
+def sum(a,b):
+    c= a + b
+    return c
+
+a = int(input('Enter the value of A : '))
+b = int(input('Enter the value of B : '))
+c = sum(a,b)
+print('The value of C is : ',c)

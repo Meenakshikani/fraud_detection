f1638bc767ae8ccda9471c175e5469cd2f73d164
Meenakshikani <meenakshi02232020@gmail.com>
2021-12-04 12:09:48 -0600

    adding multiplication and division

--- Data_Processing.py
+++ Data_Processing.py
@@ -4,7 +4,24 @@ def sum(a,b):
     c= a + b
     return c
 
+def mul(a,b):
+    d= a * b
+    return d
+
+def div(a,b):
+    e = a / b
+    return e
+
 a = int(input('Enter the value of A : '))
 b = int(input('Enter the value of B : '))
 c = sum(a,b)
 print('The value of C is : ',c)
+
+
+d = mul(a,b)
+print('The value of C is : ',d)
+
+
+e = div(a,b)
+print('The value of C is : ',e)
+
